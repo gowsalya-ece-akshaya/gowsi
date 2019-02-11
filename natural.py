1c388479319c9c72f4cfdba85067ff3bf53b3cf5
gowsalya-ece-akshaya <42996822+gowsalya-ece-akshaya@users.noreply.github.com>
2019-02-11 10:59:06 +0530

    Create natural.py

---
+++ natural.py
@@ -0,0 +1,10 @@
+num = int(input("Please Enter any Num: "))
+
+total = 0
+v= 1
+
+while (v <= num):
+    total = total + v
+    v = v + 1
+
+print("The Sum of Natural Numbers from 1 to {0} =  {1}".format(number, total))
